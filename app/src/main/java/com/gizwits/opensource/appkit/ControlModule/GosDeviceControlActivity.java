e4a25d5cee6511b753ecfc5214d20ef42e703c64
wangdongyi <wangdongyi@zmeng123.com>
2018-01-25 22:07:42 +0800

    done

--- app/src/main/java/com/gizwits/opensource/appkit/ControlModule/GosDeviceControlActivity.java
+++ app/src/main/java/com/gizwits/opensource/appkit/ControlModule/GosDeviceControlActivity.java
@@ -706,6 +706,9 @@ public class GosDeviceControlActivity extends GosControlModuleBaseActivity {
         nice_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(baseBean==null){
+                    return;
+                }
                 switch (position) {
                     case 0:
                         //手动
@@ -714,13 +717,12 @@ public class GosDeviceControlActivity extends GosControlModuleBaseActivity {
                         layout_automatic.setVisibility(View.GONE);
                         main_qualified_layout.setVisibility(View.GONE);
                         qualified_layout_double.setVisibility(View.GONE);
-                        if (baseBean.getManualBean() == null) {
-                            return;
+                        if (baseBean.getManualBean() != null) {
+                            if (baseBean.getAllSwitch())
+                                switch10.setChecked(baseBean.getManualBean().getKillSwitch());
+                            else
+                                switch10.setChecked(false);
                         }
-                        if (baseBean.getAllSwitch())
-                            switch10.setChecked(baseBean.getManualBean().getKillSwitch());
-                        else
-                            switch10.setChecked(false);
                         break;
                     case 1:
                         textView_keller.setVisibility(View.INVISIBLE);
@@ -729,39 +731,36 @@ public class GosDeviceControlActivity extends GosControlModuleBaseActivity {
                         main_qualified_layout.setVisibility(View.GONE);
                         qualified_layout_double.setVisibility(View.GONE);
                         AutomaticBean automaticBean = baseBean.getAutomaticBean();
-                        if (automaticBean == null) {
-                            return;
+                        if (automaticBean != null) {
+                            getWeek(automaticBean.getWeek());
+                            starHour = automaticBean.getStarHour();
+                            starMinute = automaticBean.getStarMinute();
+                            endHour = automaticBean.getEndHour();
+                            endMinute = automaticBean.getEndMinute();
+                            textView_start_time.setText((starHour < 10 ? "0" + starHour : starHour) + ":" + (starMinute < 10 ? "0" + starMinute : starMinute));
+                            textView_end_time.setText((endHour < 10 ? "0" + endHour : endHour) + ":" + (endMinute < 10 ? "0" + endMinute : endMinute));
                         }
-                        getWeek(automaticBean.getWeek());
-                        starHour = automaticBean.getStarHour();
-                        starMinute = automaticBean.getStarMinute();
-                        endHour = automaticBean.getEndHour();
-                        endMinute = automaticBean.getEndMinute();
-                        textView_start_time.setText((starHour < 10 ? "0" + starHour : starHour) + ":" + (starMinute < 10 ? "0" + starMinute : starMinute));
-                        textView_end_time.setText((endHour < 10 ? "0" + endHour : endHour) + ":" + (endMinute < 10 ? "0" + endMinute : endMinute));
                         break;
                     case 2:
                         textView_keller.setVisibility(View.INVISIBLE);
                         switch10.setVisibility(View.INVISIBLE);
                         layout_automatic.setVisibility(View.GONE);
                         TimingBean timingBean = baseBean.getTimingBean();
-                        if (timingBean == null) {
-                            return;
-                        }
-
-                        timingHour = timingBean.getHour();
-                        timingMinute = timingBean.getMinute();
-                        if (type == 1) {
-                            qualified_layout_double.setVisibility(View.VISIBLE);
-                            main_qualified_layout.setVisibility(View.GONE);
-                            int hourR = timingBean.getHourRight();
-                            int minR = timingBean.getMinuteRight();
-                            textView_qualified_time_left.setText((timingHour < 10 ? "0" + timingHour : timingHour) + ":" + (timingMinute < 10 ? "0" + timingMinute : timingMinute));
-                            textView_qualified_time_right.setText((hourR < 10 ? "0" + hourR : hourR) + ":" + (minR < 10 ? "0" + minR : minR));
-                        } else {
-                            qualified_layout_double.setVisibility(View.GONE);
-                            main_qualified_layout.setVisibility(View.VISIBLE);
-                            textView_qualified_time.setText((timingHour < 10 ? "0" + timingHour : timingHour) + ":" + (timingMinute < 10 ? "0" + timingMinute : timingMinute));
+                        if (timingBean != null) {
+                            timingHour = timingBean.getHour();
+                            timingMinute = timingBean.getMinute();
+                            if (type == 1) {
+                                qualified_layout_double.setVisibility(View.VISIBLE);
+                                main_qualified_layout.setVisibility(View.GONE);
+                                int hourR = timingBean.getHourRight();
+                                int minR = timingBean.getMinuteRight();
+                                textView_qualified_time_left.setText((timingHour < 10 ? "0" + timingHour : timingHour) + ":" + (timingMinute < 10 ? "0" + timingMinute : timingMinute));
+                                textView_qualified_time_right.setText((hourR < 10 ? "0" + hourR : hourR) + ":" + (minR < 10 ? "0" + minR : minR));
+                            } else {
+                                qualified_layout_double.setVisibility(View.GONE);
+                                main_qualified_layout.setVisibility(View.VISIBLE);
+                                textView_qualified_time.setText((timingHour < 10 ? "0" + timingHour : timingHour) + ":" + (timingMinute < 10 ? "0" + timingMinute : timingMinute));
+                            }
                         }
                         break;
                 }
